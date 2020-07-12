616fbcac3eeccfca594f86f0bb00f5896a771b33
Deryam Hermosilla <deryam.19.07.13@gmail.com>
2020-07-12 00:37:05 -0500

    accediendo a la cámara del equipo

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  resultadoScan = null;
+  scanActivado = false;
+
+  //Para mostrar video
+  //Revisar documentación de ViewChild *static
+  @ViewChild('video', { static: false}) video: ElementRef;
+  elementoVideo: any;
+
+  constructor( ) { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    this.elementoVideo = this.video.nativeElement;
+  }
+
+  async iniciarScan() {
+    this.scanActivado = true;
+    //para activar la cámara
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: 'environment'}
+    });
+
+    this.elementoVideo.srcObject = stream;
+    this.elementoVideo.setAttribute('playsinline', true);
+    this.elementoVideo.play();
+  }
+
+  resetear() {
+
+  }
+
+  detenerScan() {
+    this.scanActivado = false;
+  }
 }
