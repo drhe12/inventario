6684f2665d34f26ade08dc46f3ff8e12a8a53778
Deryam Hermosilla <deryam.19.07.13@gmail.com>
2020-07-12 02:24:10 -0500

    ya lee los QR

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+//Importamos la libreria que nos ayuda a leer QR
+import jsQR from 'jsqr';
 
 @Component({
   selector: 'app-home',
@@ -10,10 +12,14 @@ export class HomeComponent implements OnInit, AfterViewInit {
   resultadoScan = null;
   scanActivado = false;
 
+  localstream: any;
   //Para mostrar video
   //Revisar documentación de ViewChild *static
-  @ViewChild('video', { static: false}) video: ElementRef;
+  @ViewChild('video', { static: false }) video: ElementRef;
+  @ViewChild('canvas', { static: true }) canvas: ElementRef;
   elementoVideo: any;
+  elementoCanvas: any;
+  contexturaCanvas: any;
 
   constructor( ) { }
 
@@ -22,6 +28,9 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     this.elementoVideo = this.video.nativeElement;
+    this.elementoCanvas = this.canvas.nativeElement;
+    this.contexturaCanvas = this.elementoCanvas.getContext('2d');
+
   }
 
   async iniciarScan() {
@@ -30,17 +39,58 @@ export class HomeComponent implements OnInit, AfterViewInit {
     const stream = await navigator.mediaDevices.getUserMedia({
       video: { facingMode: 'environment'}
     });
-
+    this.localstream = stream;
     this.elementoVideo.srcObject = stream;
     this.elementoVideo.setAttribute('playsinline', true);
     this.elementoVideo.play();
+
+    requestAnimationFrame(this.scanear.bind(this));
   }
 
-  resetear() {
+  scanear() {
+    if (this.elementoVideo.readyState === this.elementoVideo.HAVE_ENOUGH_DATA ) {
+      this.elementoCanvas.height = this.elementoVideo.videoHeight;
+      this.elementoCanvas.width = this.elementoVideo.videoWidth;
 
+      this.contexturaCanvas.drawImage(
+        this.elementoVideo,
+        0,
+        0,
+        this.elementoCanvas.width,
+        this.elementoCanvas.height
+      );
+
+      const datosImagen = this.contexturaCanvas.getImageData(
+        0,
+        0,
+        this.elementoCanvas.width,
+        this.elementoCanvas.height
+      );
+
+      const codigoQR = jsQR(datosImagen.data, datosImagen.width, datosImagen.height, {
+        inversionAttempts: 'dontInvert'
+      })
+      console.log('QR: ', codigoQR);
+
+      if ( codigoQR ) {
+
+      } else if ( this.scanActivado ) {
+      requestAnimationFrame(this.scanear.bind(this));
+      }
+
+    } else {
+      requestAnimationFrame(this.scanear.bind(this));
+    }
   }
 
   detenerScan() {
     this.scanActivado = false;
+    //desactivar la cámara
+    this.elementoVideo.pause();
+    this.localstream.getTracks()[0].stop();
+  }
+
+  resetear() {
+
   }
 }
